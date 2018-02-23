04be2352b7fc4e44e6bee3449a3707157036cb8e
Canming Huang <support@emgu.com>
2018-02-23 16:11:38 -0500

    Only use SURF for stitcher if HAVE_OPENCV_NONFREE is also enabled.

--- modules/stitching/src/stitcher.cpp
+++ modules/stitching/src/stitcher.cpp
@@ -59,7 +59,7 @@ Stitcher Stitcher::createDefault(bool try_use_gpu)
 #ifdef HAVE_OPENCV_CUDALEGACY
     if (try_use_gpu && cuda::getCudaEnabledDeviceCount() > 0)
     {
-#ifdef HAVE_OPENCV_XFEATURES2D
+#if defined(HAVE_OPENCV_XFEATURES2D) &&  defined(HAVE_OPENCV_NONFREE)
         stitcher.setFeaturesFinder(makePtr<detail::SurfFeaturesFinderGpu>());
 #else
         stitcher.setFeaturesFinder(makePtr<detail::OrbFeaturesFinder>());
@@ -70,7 +70,7 @@ Stitcher Stitcher::createDefault(bool try_use_gpu)
     else
 #endif
     {
-#ifdef HAVE_OPENCV_XFEATURES2D
+#if defined(HAVE_OPENCV_XFEATURES2D) &&  defined(HAVE_OPENCV_NONFREE)
         stitcher.setFeaturesFinder(makePtr<detail::SurfFeaturesFinder>());
 #else
         stitcher.setFeaturesFinder(makePtr<detail::OrbFeaturesFinder>());
